d3009f94b6588d8097b3720f2da778378262a6ed
Psemp <57976914+Psemp@users.noreply.github.com>
2020-05-28 12:48:59 +0200

    Update data_insertion.py

--- data_insertion.py
+++ data_insertion.py
@@ -1,7 +1,9 @@
 
 def Insert_data(mycursor, ObjectList1, ObjectList2):
     for product in ObjectList1:
-        mycursor.execute("INSERT INTO Product_table VALUES(Product_id , Product_name, Brand, Stores, Nutriscore, Category_id);")
-        product_values = ()
+        product_insertion = "INSERT INTO Product_table (Product_id , Product_name, Brand, Stores, Nutriscore, Category_id) VALUES(s%, s%, s%, s%, s%, s%)"
+        product_values = (product.barcode, product.name, product.brand, product.stores, product.nutriscore, product.category_id)
+        mycursor.execute()
+        
     for category in ObjectList2:
         mycursor.execute(f"INSERT INTO Category_table VALUES({category.id}, {category.web_id}, {category.name});")
