1915aa76fd14e887e75bc155b99f542efdc3545a
Pierre Sempere <pierre.sempere.01@gmail.com>
2020-05-30 10:10:51 +0200

    sql improvements

--- data_insertion.py
+++ data_insertion.py
@@ -1,9 +1,13 @@
 
 def Insert_data(mycursor, ObjectList1, ObjectList2):
     for product in ObjectList1:
-        product_insertion = "INSERT INTO Product_table (Product_id , Product_name, Brand, Stores, Nutriscore, Category_id) VALUES(s%, s%, s%, s%, s%, s%)"
-        product_values = (product.barcode, product.name, product.brand, product.stores, product.nutriscore, product.category_id)
-        mycursor.execute()
-        
+        product_insertion = """INSERT INTO Product_table
+        (Product_id , Product_name, Brand, Stores, Nutriscore, Category_id)
+        VALUES(%s, %s, %s, %s, %s, %s)"""
+        product_values = (product.barcode, product.name, product.brand, product.stores, product.nutriscore, product.category_id[0])
+        mycursor.execute(product_insertion, product_values)
+
     for category in ObjectList2:
-        mycursor.execute(f"INSERT INTO Category_table VALUES({category.id}, {category.web_id}, {category.name});")
+        category_insertion = "INSERT INTO Category_table (Category_id, Category_name, Translated_name) VALUES(%s, %s, %s)"
+        category_values = (category.id, category.web_id, category.display_name)
+        mycursor.execute(category_insertion, category_values)
--- testssql.py
+++ testssql.py
@@ -3,7 +3,7 @@ from sql_db_creation import Create_Dbs
 from data_insertion import Insert_data
 from demeter import product_list, category_list
 
-db = mysql.connector.connect(
+cnx = mysql.connector.connect(
     user='root',
     password="06022018",
     host="localhost",
@@ -12,8 +12,13 @@ db = mysql.connector.connect(
     auth_plugin='mysql_native_password'
     )
 
-mycursor = db.cursor()
+mycursor = cnx.cursor()
 
 Create_Dbs(mycursor)
 
 Insert_data(mycursor, product_list, category_list)
+
+cnx.commit()
+
+mycursor.close()
+cnx.close()
