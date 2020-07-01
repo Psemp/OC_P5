f3a85e8d68bb2513d57d44a9d97160c62f88aafe
Pierre Sempere <pierre.sempere.01@gmail.com>
2020-07-01 11:36:14 +0200

    tests

---
+++ tests/input_regulation.py
@@ -0,0 +1,35 @@
+
+def InputChecker(usage, minimum, maximum, input_text):
+    user_input = ""
+    valid_input = False
+    if usage == "ls_ind":
+        while valid_input is False:
+            print(input_text)
+            user_input = IntChecker(user_input, minimum, maximum)
+            if user_input >= minimum and user_input <= maximum:
+                return user_input
+                # valid_input = True  # Return user_input is better
+            else:
+                user_input = input(f"""Please enter a value between {minimum} and {maximum} : """)
+
+    if usage == "y_n":
+        while valid_input is False:
+            user_input = input(input_text)
+            if user_input == "y" or user_input == "n":
+                valid_input = True  # Useless
+                return user_input
+            else:
+                user_input = input("Please answer y or n : ")
+
+
+def IntChecker(user_input, minimum, maximum):
+    type_check = False
+    error_message = "Please Enter a number"
+    while type_check is False:
+        user_input = input(f"""Please enter a value between {minimum} and {maximum} : """)
+        try:
+            int(user_input)
+            type_check = True
+            return int(user_input)
+        except ValueError:
+            print(error_message)
---
+++ tests/test_app.py
@@ -0,0 +1,56 @@
+import mysql.connector
+from display_queries_tst import CategorySelection, ProductSelection
+
+usr_pwd = input("Please enter password : ")
+
+cnx = mysql.connector.connect(
+    user='root',
+    password=usr_pwd,
+    host="localhost",
+    passwd=usr_pwd,
+    database="Project5_db",
+    auth_plugin='mysql_native_password'
+    )
+
+mycursor = cnx.cursor()
+
+u_category_choice = 0
+u_category_id = 0
+u_product_choice = 0
+u_id_of_selection = 0
+u_id_of_substitute = 0
+u_products_seen = 0
+u_categories_seen = 0
+
+
+displayed_categories = []
+displayed_products = []
+display_limit = 10
+
+
+def Insert_data(mycursor, ObjectList1, ObjectList2):
+
+    for category in ObjectList2:
+        category_insertion = """INSERT INTO Category_table
+        (Category_id, Category_name, Translated_name)
+        VALUES(%s, %s, %s)"""
+        category_values = (category.id, category.web_id, category.display_name)
+        mycursor.execute(category_insertion, category_values)
+    cnx.commit()
+
+    for product in ObjectList1:
+        product_insertion = """INSERT INTO Product_table
+        (Product_id , Product_name, Brand,Stores,Url,Nutriscore,Category_id)
+        VALUES(%s, %s, %s, %s, %s, %s, %s)"""
+        product_values = (product.barcode, product.name, product.brand, product.stores, product.url, product.nutriscore, product.category_id[0])
+        mycursor.execute(product_insertion, product_values)
+    cnx.commit()
+
+
+# print(type(CategorySelection(mycursor, displayed_categories, u_category_choice, u_categories_seen)))
+print(CategorySelection(mycursor, displayed_categories, u_category_choice, u_categories_seen))
+# u_category_choice = CategorySelection(mycursor, displayed_categories, u_category_choice, u_categories_seen)
+# print(u_category_choice)
+# u_category_choice = ''.join(u_category_choice[0])
+# print(u_category_choice)
+# print(ProductSelection(mycursor, u_category_choice, displayed_products, u_products_seen))
